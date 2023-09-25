6e96c2c5a565e524b60ec327000c8f1f80c090a0
kelvinvu19 <TrungKien.Vu@edu.sait.ca>
2023-09-25 11:42:53 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,21 +1,15 @@
-import React from 'react';
 import Link from 'next/link';
 
-export default function StudentInfo() {
+ 
+
+export default function Page() {
   return (
-    <div>
-      <YourName />
+<div>
+<h1>CPRG 306: Web Development 2 - Assignments</h1>
+<p>    
+<Link href="https://github.com/kelvinvu19/cprg306-assignments"> https://github.com/kelvinvu19/cprg306-assignments </Link>
+</p>
+</div>
 
-const HomePage = <h1>CPRG 306: Web Development 2 - Assignments</h1>
-
-function CourseSection() {
-  return <h1>CPRG 306 B</h1>;
-}
-
-function GitHubRepository() {
-  return (
-    <p>    
-      <Link href="https://github.com/kelvinvu19/cprg306-assignments"> https://github.com/kelvinvu19/cprg306-assignments </Link>
-    </p>
-  );
+    );
 }
