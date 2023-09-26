9ba96b4267fab7e1260dfeeaf054c9400cf197d9
kelvinvu19 <TrungKien.Vu@edu.sait.ca>
2023-09-25 18:35:48 -0600

    Update page.js

--- app/week2/page.js
+++ app/week2/page.js
@@ -3,10 +3,10 @@ import Link from 'next/link';
 import StudentInfo from '../StudentInfo';
 
 export default function Week2() {
-    return (
-        <div>
+  return (
+      <div>
           <h1>My Shopping List</h1>
           <StudentInfo />
-        </div>
-      );
-    }
+      </div>
+  );
+}
