f7659fa30537e39634a97f2ce4031359b22dd4e0
kelvinvu19 <TrungKien.Vu@edu.sait.ca>
2023-09-25 18:38:45 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,12 +3,12 @@ import Link from 'next/link';
 import StudentInfo from './StudentInfo';
 
 
-const HomePage = <h1>CPRG 306: Web Development 2 - Assignments</h1>
-const Page = () => {
-    return (
-        <div>
-            <StudentInfo />
-            <Link href="/week2"> week2 </Link>
-        </div>
-    );
+export default function Week2() {
+  return (
+      <div>
+          <h1>CPRG 306: Web Development 2 - Assignments</h1>
+          <StudentInfo />
+          <Link href="/week2"> week2 </Link>
+      </div>
+  );
 }
