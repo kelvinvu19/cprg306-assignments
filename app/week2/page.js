ed68156dd6c91fd01238d3168a1e164c53ec05ad
kelvinvu19 <TrungKien.Vu@edu.sait.ca>
2023-09-25 18:46:26 -0600

    Update page.js

--- app/week2/page.js
+++ app/week2/page.js
@@ -6,7 +6,9 @@ export default function Week2() {
   return (
       <div>
           <h1>My Shopping List</h1>
-          <StudentInfo />
+          <p> Name: Kelvin Vu</p>
+          <h1>CPRG 306 B</h1>
+          <Link href="https://github.com/kelvinvu19/cprg306-assignments"> https://github.com/kelvinvu19/cprg306-assignments </Link>
       </div>
   );
 }
