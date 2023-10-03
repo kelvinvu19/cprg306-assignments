a7894aac13ca49d4897a76be21e64d12d8a191ef
kelvinvu19 <TrungKien.Vu@edu.sait.ca>
2023-10-02 20:56:08 -0600

    lab 3

--- app/week3/item.js
+++ app/week3/item.js
@@ -1,3 +1,4 @@
+import React from 'react';
 export default function Item({ name, quantity,category }) {
     return (
       <li className="border border-sky-500 bg-sky-800 w-full max-w-xs m-4 p-2">
