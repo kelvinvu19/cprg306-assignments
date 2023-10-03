00af43803e3956acbe6481c0734f0144377e95b6
kelvinvu19 <TrungKien.Vu@edu.sait.ca>
2023-10-02 21:14:38 -0600

    Update page.js

--- app/week3/page.js
+++ app/week3/page.js
@@ -4,7 +4,7 @@ import ItemList from "./item-list";
 export default function Page() {
   return (
     <main>
-      <h1 className="text-3xl font-semibold mb-4 bg-gray-200 min-h-screen p-4 ">
+      <h1 className="text-3xl font-semibold ">
       Shopping List
       </h1>
       <ItemList />
