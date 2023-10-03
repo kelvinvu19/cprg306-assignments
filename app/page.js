e16e76018828ed6ac2e5a09f9069a73c287376ef
kelvinvu19 <TrungKien.Vu@edu.sait.ca>
2023-10-02 21:06:47 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -9,6 +9,7 @@ export default function Week2() {
           <h1>CPRG 306: Web Development 2 - Assignments</h1>
           <StudentInfo />
           <Link href="/week2"> week2 </Link>
+          <Link href="/week3"> week3 </Link>
       </div>
   );
 }
