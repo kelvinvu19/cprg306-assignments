11f9730e4edd451157084027032f00c0664fafda
kelvinvu19 <TrungKien.Vu@edu.sait.ca>
2023-10-23 19:18:00 -0600

    Update new-item.js

--- app/week6/new-item.js
+++ app/week6/new-item.js
@@ -61,10 +61,18 @@ export default function NewItem({ onAddItem }) {
             onChange={handleCategoryChange}
             className="text-black"
           >
-            <option value="produce">Produce</option>
-            <option value="dairy">Dairy</option>
-            <option value="meat">Meat</option>
-            {/* Add more categories as needed */}
+            <option value="produce">produce</option>
+            <option value="dairy">dairy</option>
+            <option value="bakery">bakery</option>
+            <option value="meat">meat</option>            
+            <option value="frozen foods">frozen</option>
+            <option value="canned goods">canned</option>
+            <option value="dry goods">dry</option>
+            <option value="beverages">beverages</option>
+            <option value="snacks">snacks</option>
+            <option value="house hold">house</option>
+            <option value="other">other</option>
+
           </select>
         </label>
         <button type="submit" className="small-button">
