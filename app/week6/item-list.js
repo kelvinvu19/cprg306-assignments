c6411c2bb3227bd5129508f6092e94cc142cd4cc
kelvinvu19 <TrungKien.Vu@edu.sait.ca>
2023-10-23 19:01:35 -0600

    Update item-list.js

--- app/week6/item-list.js
+++ app/week6/item-list.js
@@ -1,35 +1,49 @@
-import React,
-{ useState } from 'react';
+"use client";
+import React, { useState } from 'react';
 import Item from './item';
+import itemsData from './items.json';
 
-export default function ItemList({ items }) {
-  const [sortBy, setSortBy] = useState('name');
+export default function ItemList() {
+  const [items] = useState(itemsData);
+  const [sortBy, setSortBy] = useState("name"); 
+
 
   const handleSortChange = (newSortBy) => {
     setSortBy(newSortBy);
   };
+
+
   const sortedItems = [...items].sort((a, b) => {
-    if (sortBy === 'name') {
+    if (sortBy === "name") {
       return a.name.localeCompare(b.name);
-    } else if (sortBy === 'category') {
+    } else if (sortBy === "category") {
       return a.category.localeCompare(b.category);
     }
     return 0;
   });
+
+
   const nameButtonStyle = {
-    backgroundColor: sortBy === 'name' ? 'lightblue' : 'white',
+    backgroundColor: sortBy === "name" ? "lightblue" : "white",
   };
 
   const categoryButtonStyle = {
-    backgroundColor: sortBy === 'category' ? 'lightblue' : 'white',
+    backgroundColor: sortBy === "category" ? "lightblue" : "white",
   };
+
   return (
     <div>
       <div>
-        <button onClick={() => handleSortChange('name')} style={nameButtonStyle}>
+        <button
+          onClick={() => handleSortChange("name")}
+          style={nameButtonStyle}
+        >
           Sort by Name
         </button>
-        <button onClick={() => handleSortChange('category')} style={categoryButtonStyle}>
+        <button
+          onClick={() => handleSortChange("category")}
+          style={categoryButtonStyle}
+        >
           Sort by Category
         </button>
       </div>
