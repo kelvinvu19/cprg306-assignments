0e7efbedd5ad4de3b2cb11921b3100005bdd461f
kelvinvu19 <TrungKien.Vu@edu.sait.ca>
2023-10-23 20:22:03 -0600

    Update page.js

--- app/week6/page.js
+++ app/week6/page.js
@@ -1,10 +1,11 @@
-"use client" 
 import React, { useState } from 'react';
 import ItemList from "./item-list";
-import NewItem from "./new-item"; 
+import NewItem from "./new-item";
+import itemsData from './items.json';
 
 export default function Page() {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(itemsData); 
+
   const handleAddItem = (newItem) => {
     setItems([...items, newItem]);
   }
