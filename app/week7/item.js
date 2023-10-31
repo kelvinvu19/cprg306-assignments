be437e22de939575f3a17689a10b0a4137cd2dc3
kelvinvu19 <TrungKien.Vu@edu.sait.ca>
2023-10-30 18:59:11 -0600

    Update item.js

--- app/week7/item.js
+++ app/week7/item.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 export default function Item({ name, quantity, category, onSelect }) {
+  const handleClick = () => {
+    console.log('Item clicked:', name);
+    onSelect(name);
+  };
+
   return (
     <li
       className="border border-sky-500 bg-sky-800 w-full max-w-xs m-4 p-2"
-      onClick={() => onSelect(name)}
+      onClick={handleClick}
     >
       <p className="text-xl font-bold">{name}</p>
       <p>Quantity: {quantity}</p>
