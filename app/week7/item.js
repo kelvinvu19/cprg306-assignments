1366f27ba0cc6c4adaa0446fca8d43fd72d1a9ad
kelvinvu19 <TrungKien.Vu@edu.sait.ca>
2023-11-06 17:38:06 -0700

    Update item.js

--- app/week7/item.js
+++ app/week7/item.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 export default function Item({name, quantity, category, onSelect}) {
   return (
       <ul className="p-2 m-4 bg-slate-900 max-w-sm hover:bg-blue-500" onClick={() => onSelect(name)}>
@@ -5,5 +6,19 @@ export default function Item({name, quantity, category, onSelect}) {
           <li>{quantity}</li>
           <li>{category}</li>
       </ul>
+=======
+import React from 'react';
+
+export default function Item({ name, quantity, category, onSelect }) {
+  return (
+    <li
+      className="border border-sky-500 bg-sky-800 w-full max-w-xs m-4 p-2"
+      onClick={() => onSelect(name)}
+    >
+      <p className="text-xl font-bold">{name}</p>
+      <p>Quantity: {quantity}</p>
+      <p>Category: {category}</p>
+    </li>
+>>>>>>> parent of be437e2 (Update item.js)
   );
 }
