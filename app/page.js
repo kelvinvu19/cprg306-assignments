b83f1f3f54da6561f9f760f41d568757bafd5c09
kelvinvu19 <TrungKien.Vu@edu.sait.ca>
2023-12-10 18:26:11 -0700

    project

--- app/page.js
+++ app/page.js
@@ -8,14 +8,6 @@ export default function Week2() {
       <div>
           <h1>CPRG 306: Web Development 2 - Assignments</h1>
           <StudentInfo />
-          <Link href="/week2"> week2 </Link>
-          <Link href="/week3"> week3 </Link>
-          <Link href="/week4"> week4 </Link>
-          <Link href="/week5"> week5 </Link>
-          <Link href="/week6"> week6 </Link>
-          <Link href="/week7"> week7 </Link>
-          <Link href="/week8"> week8 </Link>
-          <Link href="/week10"> week10 </Link>
           <Link href="/project"> project </Link>
       </div>
   );
