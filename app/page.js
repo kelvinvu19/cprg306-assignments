3900e9388918f908629293788e6113acecff44df
kelvinvu19 <TrungKien.Vu@edu.sait.ca>
2023-12-10 18:45:04 -0700

    project

--- app/page.js
+++ app/page.js
@@ -8,7 +8,6 @@ export default function Week2() {
       <div>
           <h1>CPRG 306: Web Development 2 - Assignments</h1>
           <StudentInfo />
-          <Link href="/week2"> week2 </Link>
           <Link href="/project"> project </Link>
       </div>
   );
